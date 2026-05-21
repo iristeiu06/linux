2502622b7f1276d7814eb1f1b231a0ef16e2a800
Jorge Marques <jorge.marques@analog.com>
2026-05-21 15:09:10 +0200

    remoteproc: adi: Use stub for disable|enable_spu()
    Despite being a stub, add the placeholder at the correct location for
    future implementation.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- drivers/remoteproc/adi_remoteproc.c
+++ drivers/remoteproc/adi_remoteproc.c
@@ -30,6 +30,7 @@
 
 #include <linux/soc/adi/icc.h>
 #include <linux/soc/adi/rcu.h>
+#include <linux/soc/adi/spu.h>
 #include "remoteproc_internal.h"
 
 /* location of bootrom that loops idle */
@@ -360,10 +361,14 @@ static int adi_valid_firmware(struct rproc *rproc, const struct firmware *fw)
 
 static void enable_spu(void)
 {
+	adi_spu_set_securep(SPU_MDMA0_SRC_ID, true);
+	adi_spu_set_securep(SPU_MDMA0_DST_ID, true);
 }
 
 static void disable_spu(void)
 {
+	adi_spu_set_securep(SPU_MDMA0_SRC_ID, false);
+	adi_spu_set_securep(SPU_MDMA0_DST_ID, false);
 }
 
 static int adi_ldr_load(struct adi_rproc_data *rproc_data,
