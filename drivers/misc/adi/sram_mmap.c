25710bcd6872089618764f05da667b612e892d59
Jorge Marques <jorge.marques@analog.com>
2026-05-21 17:47:42 +0200

    fixup! misc: sram: adi: Add drivers for ADSP-SCxxx SoCs
    Isolate arch-specific calls.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- drivers/misc/adi/sram_mmap.c
+++ drivers/misc/adi/sram_mmap.c
@@ -96,8 +96,10 @@ static int sram_mmap(struct file *fp, struct vm_area_struct *vma)
 	}
 
 	fp->f_mapping->a_ops = &sram_aops;
+#ifdef CONFIG_ARM64
 	vma->vm_page_prot = __pgprot_modify(vma->vm_page_prot, PTE_ATTRINDX_MASK,
 		PTE_ATTRINDX(MT_NORMAL) | PTE_PXN | PTE_UXN);
+#endif
 	vma->vm_private_data = sram;
 	vma->vm_ops = NULL;
 
